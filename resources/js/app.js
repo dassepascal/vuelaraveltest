d1960df8d3891210e369d91444a88dfa28a84b74
dassepascal <pascaldasse56@gmail.com>
2024-01-20 08:39:11 +0100

    vue install

--- resources/js/app.js
+++ resources/js/app.js
@@ -1,7 +1,14 @@
 import './bootstrap';
+import { createApp } from 'vue';
+import Counter from './components/Counter.vue';
 
 import Alpine from 'alpinejs';
 
 window.Alpine = Alpine;
 
 Alpine.start();
+
+const app = createApp({});
+
+app.component('counter', Counter);
+app.mount('#app');
---
+++ resources/js/components/Counter.vue
@@ -0,0 +1,26 @@
+<script setup>
+    import { ref } from 'vue'
+
+    const count = ref(0)
+</script>
+
+<template>
+    <div>hello</div>
+    {{ count }}
+
+    <button class="btn btn-blue" @click="count++">
+        Add
+    </button>
+</template>
+
+<style>
+.btn{
+    @apply font-bold py-2 px-4 rounded;
+}
+.btn-blue{
+    @apply bg-blue-500 text-white;
+}
+.btn-blue:hover {
+    @apply bg-blue-700;
+  }
+</style>
