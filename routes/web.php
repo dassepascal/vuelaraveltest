776ea82d83fd71ec5ef9b641a17029f78f9141e1
dassepascal <pascaldasse56@gmail.com>
2024-01-20 10:33:20 +0100

    route product

--- routes/web.php
+++ routes/web.php
@@ -1,6 +1,8 @@
 <?php
 
+use App\Http\Controllers\ProductController;
 use App\Http\Controllers\ProfileController;
+use App\Models\Product;
 use Illuminate\Support\Facades\Route;
 
 /*
@@ -29,3 +31,5 @@ Route::middleware('auth')->group(function () {
 });
 
 require __DIR__.'/auth.php';
+
+Route::get('/products',[ProductController::class,'index'])->name('products.index');
